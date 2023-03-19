a025b126d623cbd31697d424b8ab212dc9b9a714
maltunok <m_altunok@hotmail.com>
2023-03-19 21:20:05 +0300

    update

--- FingerCounterApp.py
+++ FingerCounterApp.py
@@ -2,7 +2,7 @@ import cv2
 import time
 import os
 import HandTrackingModule as htm
-wCam, hCam = 640, 480
+wCam, hCam = 1024, 1024
 cap = cv2.VideoCapture(0)
 cap.set(3, wCam)
 cap.set(4, hCam)
@@ -17,11 +17,11 @@ for imPath in myList:
 print(len(overlayList))
 pTime = 0
 detector = htm.handDetector(detectionCon=0.75)
-tipIds = [4, 8, 12, 16, 20] # 4 thumb, 8 index, 12 middle, 16 ring, 20 pinky
+tipIds = [4, 8, 12, 16, 20]  # 4 thumb, 8 index, 12 middle, 16 ring, 20 pinky
 while True:
     success, img = cap.read()
     img = detector.findHands(img)
-   
+
     lmList = detector.findPosition(img, draw=False)
     # print(lmList)
     if len(lmList) != 0:
@@ -34,17 +34,17 @@ while True:
             fingers.append(0)
 
         # 4 fingers
-        for id in range(1,5):
+        for id in range(1, 5):
             if lmList[tipIds[id]][2] < lmList[tipIds[id]-2][2]:
                 fingers.append(1)
             else:
                 fingers.append(0)
-        #print(fingers)
+        # print(fingers)
         totalFingers = fingers.count(1)
         print(totalFingers)
 
         h, w, c = overlayList[totalFingers-1].shape
-        img[0:h, 0:w] = overlayList[totalFingers-1]
+        # img[0:h, 0:w] = overlayList[totalFingers-1]
         cv2.rectangle(img, (20, 225), (170, 425), (0, 255, 0), cv2.FILLED)
         cv2.putText(img, str(totalFingers), (45, 375), cv2.FONT_HERSHEY_PLAIN,
                     10, (255, 0, 0), 25)
@@ -54,6 +54,7 @@ while True:
     cTime = time.time()
     fps = 1 / (cTime - pTime)
     pTime = cTime
-    cv2.putText(img, f'FPS: {int(fps)}', (400, 70), cv2.FONT_HERSHEY_PLAIN,3, (255, 0, 0), 3)
+    cv2.putText(img, f'FPS: {int(fps)}', (400, 70),
+                cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
     cv2.imshow("Image", img)
     cv2.waitKey(1)
